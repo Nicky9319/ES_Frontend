d0c65b723b3063f089b028bbdd85675f35b5bd49
Nicky9319 <paarthsaxena2005@gmail.com>
2025-04-14 07:19:25 +0530

    Add initial components for Choose Persona, Landing, Mentor Profile Creation, and User Profile Creation pages

---
+++ src/Views/Choose Persona Page/choosePersonaPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ChoosePersonaPage() {
+    return (
+        <div>
+            <h1>Choose Persona Page</h1>
+        </div>
+    );
+}
+
+export default ChoosePersonaPage;
