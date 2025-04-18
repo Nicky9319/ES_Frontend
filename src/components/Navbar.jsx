f417ddd77e16e95e027403f2955276db63159977
Raghav Aloria <raghavaloria@gmail.com>
2025-04-18 07:57:13 +0530

    spell check

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -104,7 +104,7 @@ const Navbar = () => {
                       onClick={(e) => handleNavigation("/es-tiers", e)}
                       className="text-[#E0E0E0] no-underline hover:font-bold hover:text-[#EE8631] transition-all text-base px-4 whitespace-nowrap"
                     >
-                      yet ES Tiers
+                      ES Tiers
                     </a>
                   </li>
                   <li className="md:block hidden h-5 w-px bg-[#AD662F]"></li>
